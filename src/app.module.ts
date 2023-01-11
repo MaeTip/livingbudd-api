e9240f194d6277ca273efeee4967fd5a4cdb2c85
Numpon Maytangsuphut <numpon014@gmail.com>
2023-01-11 20:14:36 +0700

    Introduced auth, users, admin, and room modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { RoomModule } from './room/room.module';
+import { AdminUserModule } from './admin-user/admin-user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, RoomModule, AdminUserModule],
   controllers: [AppController],
   providers: [AppService],
 })
